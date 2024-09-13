3a57135594ea0123eef9d67d975cbf43997658d7
nat-chin <138374958+nat-chin@users.noreply.github.com>
2024-09-13 17:18:22 +0700

    Initial

---
+++ src/BMS.cpp
@@ -0,0 +1,49 @@
+#include <Arduino.h>
+#include <SPI.h>
+#include <mcp2515.h>
+
+struct can_frame canMsgRec;
+struct can_frame canMsgSend;
+MCP2515 mcp2515(10);
+
+
+void setup() {
+  Serial.begin(115200);
+  canMsgSend.can_id  = 0x01;
+  canMsgSend.can_dlc = 2;
+  canMsgSend.data[0] = 0x0E;
+  canMsgSend.data[1] = 0x0F;
+  
+  mcp2515.reset(); // Reset SPI 
+  mcp2515.setBitrate(CAN_500KBPS);
+  mcp2515.setNormalMode();
+  
+  Serial.println("------- CAN Read ----------");
+  Serial.println("ID  DLC   DATA");
+}
+
+unsigned long last_time = 0;
+
+void loop() {
+  
+  
+  if(millis()-last_time == 500) {
+    mcp2515.sendMessage(&canMsgSend);
+    // Serial.println("Messages sent");
+
+    if (mcp2515.readMessage(&canMsgRec) == MCP2515::ERROR_OK) {
+    Serial.print(canMsgRec.can_id, HEX); // print ID
+    Serial.print(" "); 
+    Serial.print(canMsgRec.can_dlc, HEX); // print DLC
+    Serial.print(" ");
+    
+    for (int i = 0; i<canMsgRec.can_dlc; i++)  {  // print the data
+      Serial.print(canMsgRec.data[i],HEX);
+      Serial.print(" ");
+    }
+
+    Serial.println();      
+    }
+    
+  }
+}
---
+++ src/Charger.cpp
@@ -0,0 +1,52 @@
+#include <Arduino.h>
+#include <SPI.h>
+#include <mcp2515.h>
+
+struct can_frame canMsgRec;
+struct can_frame canMsgSend;
+
+MCP2515 mcp2515(10);
+
+
+void setup() {
+  canMsgSend.can_id  = 0x02;
+  canMsgSend.can_dlc = 2;
+  canMsgSend.data[0] = 0xAA;
+  canMsgSend.data[1] = 0xBB;
+
+  while (!Serial);
+  Serial.begin(115200);
+  
+  mcp2515.reset();
+  mcp2515.setBitrate(CAN_500KBPS);
+  mcp2515.setNormalMode();
+  
+  Serial.println("------- CAN Read ----------");
+  Serial.println("ID  DLC   DATA");
+}
+
+unsigned long last_time = 0;
+
+void loop() {
+
+  
+  if(millis()-last_time == 500) {
+    if (mcp2515.readMessage(&canMsgRec) == MCP2515::ERROR_OK) {
+    Serial.print(canMsgRec.can_id, HEX); // print ID
+    Serial.print(" "); 
+    Serial.print(canMsgRec.can_dlc, HEX); // print DLC
+    Serial.print(" ");
+    
+    for (int i = 0; i<canMsgRec.can_dlc; i++)  {  // print the data
+      Serial.print(canMsgRec.data[i],HEX);
+      Serial.print(" ");
+    }
+
+    Serial.println();      
+    }
+    mcp2515.sendMessage(&canMsgSend);
+    // Serial.println("Messages sent");
+  }
+  
+  
+}
