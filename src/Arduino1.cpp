5f02e12600fed7d56f14f4dd742e3fb4f5f7bb33
nat-chin <138374958+nat-chin@users.noreply.github.com>
2024-10-28 00:20:33 +0700

    Test Successed

--- src/Arduino1.cpp
+++ src/Arduino1.cpp
@@ -13,7 +13,7 @@
 
 can_frame bmssent;
 can_frame obcreceived;
-MCP2515 mcp2515(10);
+MCP2515 mcp2515(10,MCP_16MHZ);
 #define SDCPIN 13 // actual test will use pin4
 
 void setup() {
@@ -21,7 +21,7 @@ void setup() {
   digitalWrite(SDCPIN,HIGH);
   Serial.begin(115200);
   /* Set up BMS CAN frame*/
-    bmssent.can_id  = 0x1806E5F4;
+    bmssent.can_id  = 0x1806E5F4 | CAN_EFF_FLAG;
     bmssent.can_dlc = 8;
     // Conserved Byte
     bmssent.data[5] = 0x00;
@@ -38,83 +38,73 @@ void setup() {
 }
 
 unsigned long last_time = 0;
-struct status {
-  uint8_t statbin[8];
-  bool shutdownsig = 1; // Default should be 1 = OK , 0 = SHUTDOWN
-};
+// struct status {
+//   uint8_t statbin[8];
+//   bool shutdownsig = 1; // Default should be 1 = OK , 0 = SHUTDOWN
+// };
 void loop() {
   
   // /* BMS Native Feature*/
-  //   float cellvolt = 5;
-  //   float dischgAmp = 5;
-  //   // Function to read individual Cell voltage once
-  //   Serial.println();
-  //   Serial.print("Cell Voltage: "); Serial.println(cellvolt,DEC);
+    //   float cellvolt = 5;
+    //   float dischgAmp = 5;
+    //   // Function to read individual Cell voltage once
+    //   Serial.println();
+    //   Serial.print("Cell Voltage: "); Serial.println(cellvolt,DEC);
 
   /* Minimum Voltage of 2 Module : 3.1*20 = 62V , Nominal Volage 3.6*20 = 72V
   Maximum allowable Voltage for 2 module : 83V => 830 => 0x 03 3E
   Maximum allowable current for 2 module : 5A => 50 => 0x 00 32 */
   // Polling the Message From on board charger (Every 500 ms)
-  /* Use PCANView to monitor the Bus instead */
-    // Serial.print("ID: "); Serial.println(canMsgRec.can_id, HEX); 
-    // Serial.print("DLC: ");Serial.println(canMsgRec.can_dlc, HEX);
-    // Serial.print("Data(Bytes): ");
-    // for(int i = 0; i < canMsgRec.can_dlc; i++) {
-    //   Serial.print(canMsgRec.data[i],HEX); Serial.print(" ");
-    // } Serial.println(); 
   status STAT;
   // float OBCVolt = 0;
   // float OBCAmp = 0;
 
   // Transmitting Control Message (Every 500ms as well)
-  if(millis()-last_time == 500) {
-    
+  if(millis()-last_time >= 500) {
     
     // This can be problematic as there needs to be a first message to make the OBC not entering COMMUNICATION ERROR
-    // Should I condition this as well , or just leave it be??
+    // make sure bms both send first then recieve within 500ms. 
     // Condition 1 Normal BMS message during charge
-    bmssent.data[0] = 0x03; // V highbyte 
-    bmssent.data[1] = 0x3E; // V lowbyte
-    bmssent.data[2] = 0x00; // A Highbyte
-    bmssent.data[3] = 0x32; // A Lowbyte
-    bmssent.data[4] = 0x00; // Control Byte 0 charger operate
-    // Not sure if there should be any change to V,A cap limit , just fixed the number by now
-    // Send TTL LOW to Charging shutdown (Passed Through opto) [/]
-    mcp2515.sendMessage(&bmssent);
-
-    // Condition 2 Message During Shutdown
-    if(STAT.shutdownsig == 0){
+    if(STAT.shutdownsig == 1) {
+      bmssent.data[0] = 0x03; // V highbyte 
+      bmssent.data[1] = 0x3E; // V lowbyte 83.0 V fake data
+      bmssent.data[2] = 0x00; // A Highbyte
+      bmssent.data[3] = 0x32; // A Lowbyte 5.0 A fake data
+      bmssent.data[4] = 0x00; // Control Byte 0 charger operate
+      // Not sure if there should be any change to V,A cap limit , just fixed the number by now
+      // Send TTL LOW to Charging shutdown (Passed through builtin Relay driver there, fine.)
+    } else {
+      // Condition 2 Message During Shutdown
       bmssent.data[0] = 0x00; // V highbyte 
       bmssent.data[1] = 0x00; // V lowbyte
       bmssent.data[2] = 0x00; // A Highbyte
       bmssent.data[3] = 0x00; // A Lowbyte
       bmssent.data[4] = 0x01; // Control Byte 1 charger shutdown
       // Turn maximum allowable voltage and current to 0 also as fail safe
-      mcp2515.sendMessage(&bmssent);
-      Serial.println();
-    }
+    } mcp2515.sendMessage(MCP2515::TXB1,&bmssent);
   
     if (mcp2515.readMessage(&obcreceived) == MCP2515::ERROR_OK) {
-      Serial.print("ID: "); Serial.println(obcreceived.can_id, HEX); 
+      uint32_t parsedEXTId = obcreceived.can_id & ~CAN_EFF_FLAG;
+      Serial.print("ID: "); Serial.println(parsedEXTId, HEX); 
       Serial.print("DLC: ");Serial.println(obcreceived.can_dlc, HEX);
       Serial.print("Data(Bytes): ");
       for(int i = 0; i < obcreceived.can_dlc; i++) {
         Serial.print(obcreceived.data[i],HEX); Serial.print(" ");
-      } Serial.println(); 
+      } Serial.println();
+
       // This block of code execute if detect CAN message from OBC ONLY--
-      if(obcreceived.can_id == 0x18FF50E5){
+      if(parsedEXTId== 0x18FF50E5) {
         // Monitor & Translate current Frame data
         uint8_t VoutH = obcreceived.data[0];
         uint8_t VoutL = obcreceived.data[1];
         uint8_t AoutH = obcreceived.data[2];
         uint8_t AoutL = obcreceived.data[3];
-        // Merge High byte with low byte
-        float OBCVolt = mergeHLbyte(VoutH,VoutL);
-        float OBCAmp = mergeHLbyte(AoutH,AoutL);
+        float OBCVolt = mergeHLbyte(VoutH,VoutL)*0.1;
+        float OBCAmp = mergeHLbyte(AoutH,AoutL)*0.1;
         // OBCVolt = mergeHLbyte(VoutH,VoutL);
         // OBCAmp = mergeHLbyte(AoutH,AoutL);
-        Serial.print("Charging Voltage[V]: "); Serial.println(OBCVolt,DEC);
-        Serial.print("Charging Current[A]: "); Serial.println(OBCAmp,DEC);
+        Serial.print("Voltage from OBC: "); Serial.print(OBCVolt); Serial.println("V");
+        Serial.print("Current from OBC: "); Serial.print(OBCAmp); Serial.println("A");
         
         /* Interpret OBC status, and decide on Shutdown command */
         uint8_t stat =  obcreceived.data[4]; // Status Byte
@@ -129,6 +119,7 @@ void loop() {
           Serial.print("SHUTDOWN: NOT_OK ");
           // Shutdown , send the command to OBC to stop , then stop CAN
         }
+
         // Intepret Individual bit meaning
         Serial.print("OBC status: "); 
         for (short i =0 ; i <8 ; i++)
@@ -181,19 +172,17 @@ void loop() {
             break;
         }
 
-        // How to read status byte
-        // Detect Communication Error
-        // communication timeout on the side of Charger (No command message from BMS is already dealt within OBC control system)
-        // communication timeout of 6s on the side of BMS (6s of not receiving the status update from)
-        // Charging Shutdown Emer button Should Cutout only HV or HV + Signal + LV power , if the
+          // How to read status byte
+          // Detect Communication Error
+          // communication timeout on the side of Charger (No command message from BMS is already dealt within OBC control system)
+          // communication timeout of 6s on the side of BMS (6s of not receiving the status update from)
+          // Charging Shutdown Emer button Should Cutout only HV or HV + Signal + LV power , if the
 
-        // uint8_t swverH = canMsgRec.data[5];
-        // uint8_t swverL = canMsgRec.data[6];
-        // uint8_t hwver = canMsgRec.data[7];
+          // uint8_t swverH = canMsgRec.data[5];
+          // uint8_t swverL = canMsgRec.data[6];
+          // uint8_t hwver = canMsgRec.data[7];
       }
     }
-  
+    last_time = millis();
   }
-
-
 }
